7c959f739401f671660d729f4c14b03692152ebf
DivadanAr <divadan.arya12@gmail.com>
2023-03-20 10:31:03 +0700

    Landing page, navbar & footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+  <div class="footer">
+    <div class="konten">
+      <div class="kategori">
+        <ul>
+          <li>
+            <p>Kategori</p>
+          </li>
+          <li><a href="">RPL</a></li>
+          <li><a href="">TJKT</a></li>
+          <li><a href="">BRC</a></li>
+          <li><a href="">TE</a></li>
+          <li><a href="">Animasi</a></li>
+        </ul>
+      </div>
+      <div class="help-center">
+        <ul>
+          <li>
+            <p>Help Center</p>
+          </li>
+          <li><a href="">FAQS</a></li>
+          <li><a href="">Terms & Conditions</a></li>
+          <li><a href="">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div class="school-info">
+        <ul>
+          <li>
+            <p>School Info</p>
+          </li>
+          <li><a
+              href="https://smktarunabhakti.net/index.php/whytarbak-before-import/identitas-sekolah-before-import/">About
+              Us</a>
+          </li>
+          <li><a
+              href="https://smktarunabhakti.net/index.php/struktur-kurikulum-sekolah-menengah-kejuruan-madrasah-aliyah-kejuruan-before-import/">Curicullum</a>
+          </li>
+          <li><a
+              href="https://smktarunabhakti.net/index.php/kurikulum-before-import/kompetensi-keahlian-smk-taruna-bhakti-before-import/">Subject</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="logo-copyright">
+        <img src="image/Logo TB Teks Putih 1.png" alt="SMK Taruna Bhakti" loading='lazy'>
+        <p>Copyright ©2022 SMK Taruna Bhakti </p>
+      </div>
+    </div>
+    <div class="sosmed">
+      <p>Contact Us</p>
+      <ul>
+        <li>
+          <a target="_blank" href="https://www.instagram.com/smktarunabhakti.depok/">
+            <img src="image/instagram.png" alt="">
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.youtube.com/c/SMKTarunaBhaktiDepok">
+            <img src="image/youtube.png" alt="">
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.facebook.com/smktarunabhaktidepok">
+            <img src="image/facebook.png" alt="">
+          </a></li>
+
+        <li>
+          <a target="_blank" href="https://mail.google.com/mail/u/0/?hl=en#inbox">
+            <img src="image/email.png" alt="">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts/app')
+
+@section('content')
+<div class="container">
+  <div class="hero">
+    <div class="left-hero">
+      <h1>Klik Koperasi,<br> Semua ada</h1>
+      <p>Temukan Jajanan Murah, Enak dan Sehat</p>
+      <button class="btn-jajan">Jajan Sekarang!</button>
+    </div>
+    <div class="right-hero mt-5">
+      <div class="wrapper">
+        <div class="card-minuman">
+          <div class="bg-minuman">
+            <img src="{{ asset('assets/image/minuman.png')}}" alt="">
+          </div>
+          <p>Minuman</p>
+        </div>
+        <div class="card-makanan">
+          <div class="bg-makanan">
+            <img src="{{ asset('assets/image/makanan.png')}}" alt="">
+          </div>
+          <p>Makanan</p>
+        </div>
+        <div class="card-eskrim">
+          <div class="bg-eskrim">
+            <img src="{{ asset('assets/image/eskrim.png')}}" alt="">
+          </div>
+          <p>Es Krim</p>
+        </div>
+        <div class="card-buatan">
+          <div class="bg-buatan">
+            <img src="{{ asset('assets/image/buatan.png')}}" alt="">
+          </div>
+          <p>Buatan Rumahan</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <div class="company-profile">
+      <div class="img-company">
+        <img src="{{ asset('assets/image/koprasi.png')}}" alt="">
+      </div>
+      <div class="about-company">
+        <h1>Koperasi Starbhak?</h1>
+        <p>Koperasi Starbhak adalah sebuah koperasi yang bergerak dalam bidang penjualan makanan dan minuman di
+          lingkungan sekolah. Didirikan dengan tujuan untuk memberikan pelayanan terbaik bagi para pelajar dan tenaga
+          pendidik di lingkungan sekolah, Koperasi Starbhak telah menjadi salah satu penyedia makanan dan minuman
+          terkemuka di sekolah tersebut Koperasi Starbhak menyediakan berbagai macam menu makanan dan minuman yang
+          berkualitas tinggi dan sesuai dengan standar gizi yang diperlukan untuk pertumbuhan dan perkembangan
+          pelajar. Menu-menu yang disediakan meliputi makanan ringan, makanan berat, minuman dingin dan panas, serta
+          makanan sehat untuk para pelajar yang memerlukan asupan gizi yang lebih</p>
+      </div>
+    </div>
+
+    <div class="user-experience">
+      <div class="top-experience">
+        <div class="left-experience">
+          <h1>Kenyamanan Pelanggan adalah yang Utama Bagi Kami</h1>
+        </div>
+        <div class="right-experience">
+          <div class="line"></div>
+          <p>Note : <br>
+            Kami akan memastikan pelanggan mendapatkan pelayanan terbaik</p>
+        </div>
+      </div>
+      <div class="bot-experience">
+        <div class="experience-wrap">
+          <div class="card-exp">
+            <div class="icon">
+              <img src="{{ asset('assets/image/icon.png')}}" alt="">
+            </div>
+            <div class="text">
+              <h1>Produk Baru!</h1>
+              <p>Kami menjual produk produk yang fresh dan baru</p>
+            </div>
+          </div>
+          <div class="card-exp">
+            <div class="icon">
+              <img src="{{ asset('assets/image/icon.png')}}" alt="">
+            </div>
+            <div class="text">
+              <h1>Produk Baru!</h1>
+              <p>Kami menjual produk produk yang fresh dan baru</p>
+            </div>
+          </div>
+          <div class="card-exp">
+            <div class="icon">
+              <img src="{{ asset('assets/image/icon.png')}}" alt="">
+            </div>
+            <div class="text">
+              <h1>Produk Baru!</h1>
+              <p>Kami menjual produk produk yang fresh dan baru</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="faqs">
+    <div class="header">
+      <h1>Pertanyaan Yang Banyak Ditanyakan</h1>
+      <p>Kami telah menampung pertanyaan pertanyaan yang sering ditayakan pelanggan, dan begini jawabannya</p>
+    </div>
+    <div class="wrap-faqs">
+      <div class="card-faqs">
+        <p>Min ko harga snacknya mahal mahal sih?</p>
+        <button class="btn-more">+</button>
+      </div>  
+    </div>
+  </div>
+</div>
+@endsection
